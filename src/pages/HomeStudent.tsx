eeea020dc021f428b060b32f46e0e0c6859effc5
Jonas Dias <zdias1413@gmail.com>
2024-07-26 19:40:54 -0300

    feat: make the user profile generic
    Co-authored-by: Ellen Cristina <EllenCristina10@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>

--- src/pages/HomeStudent.tsx
+++ src/pages/HomeStudent.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@components/Header/Header";
+import { UpdateStudentProfile } from "@components/UpdateStudentProfile/UpdateStudentProfile";
 import { UserProfile } from "@components/UserProfile/UserProfile";
 import { useDisclosure } from "@nextui-org/react";
 
@@ -8,7 +9,10 @@ export function HomeStudent() {
   return (
     <div>
       <Header useDisclosure={disclosure} />
-      <UserProfile updateDisclosure={disclosure} />
+      <UserProfile
+        updateDisclosure={disclosure}
+        updateProfile={<UpdateStudentProfile />}
+      />
     </div>
   );
 }
