4ef877044e76eb94c3f6493a9c027409c9b50c19
Jonas Dias <zdias1413@gmail.com>
2024-08-05 17:46:56 -0300

    feat: add assign disciplines component
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>

---
+++ src/components/AssignDisciplines/AssignDisciplines.tsx
@@ -0,0 +1,154 @@
+import { PaginationTable } from "@components/PaginationTable/PaginationTable";
+import { useError } from "@hooks/useError";
+import { useSuccess } from "@hooks/useSuccess";
+import {
+  Button,
+  Input,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  Selection as SelectionType,
+} from "@nextui-org/react";
+import { LoadingPage } from "@pages/LoadingPage";
+import { useEffect, useMemo, useState } from "react";
+import { api } from "services/api";
+import { Discipline } from "types/discipline";
+import { GroupCollege } from "types/groupCollege";
+
+type Props = {
+  groupCollege: GroupCollege;
+};
+
+export function AssignDisciplines({ groupCollege }: Props) {
+  const [disciplines, setDisciplines] = useState<Discipline[]>([]);
+  const [page, setPage] = useState(1);
+  const [filterValue, setFilterValue] = useState("");
+  const rowsPerPage = 25;
+
+  const [selectedKeys, setSelectedKeys] = useState<SelectionType>(new Set([]));
+
+  const { setError } = useError();
+  const { setSuccess } = useSuccess();
+
+  const totalPages = useMemo(() => {
+    return Math.ceil(disciplines.length / rowsPerPage);
+  }, [disciplines]);
+
+  const items = useMemo(() => {
+    if (!disciplines) return [];
+
+    const start = (page - 1) * rowsPerPage;
+    const end = page * rowsPerPage;
+
+    if (!filterValue) return disciplines.slice(start, end);
+
+    return disciplines
+      .filter((student) => {
+        return student.name.toLowerCase().includes(filterValue.toLowerCase());
+      })
+      .slice(start, end);
+  }, [disciplines, page, filterValue]);
+
+  const getSelectedValues = () => {
+    if (selectedKeys === "all") return [];
+    if (typeof selectedKeys === "object") return Array.from(selectedKeys);
+    return [];
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const disciplines: { disciplineCode: string }[] = [];
+
+    getSelectedValues()?.forEach((key) => {
+      disciplines.push({
+        disciplineCode: key as string,
+      });
+    });
+
+    const newGroupCollege = {
+      ...groupCollege,
+      disciplines: disciplines,
+    };
+
+    api
+      .put("groupCollege/update", newGroupCollege)
+      .then(() => {
+        setSuccess("Turma atualizada com sucesso!");
+      })
+      .catch((error) => {
+        setError(error.response.data);
+      });
+  };
+
+  useEffect(() => {
+    api
+      .get<Discipline[]>("discipline/all")
+      .then((response) => {
+        setDisciplines(response.data);
+      })
+      .catch((error) => {
+        setError(error.response.data);
+      });
+  }, [setError]);
+
+  if (disciplines.length === 0) return <LoadingPage />;
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-full p-4">
+        <h1 className="text-center text-2xl font-bold">
+          Atribua um Disciplinas a uma Turma
+        </h1>
+        <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit}>
+          <p>
+            <strong>Turma: {groupCollege.groupCode} </strong>
+          </p>
+
+          <Table
+            aria-label="Tabble with all disciplines"
+            className="min-h-96"
+            topContent={
+              <Input
+                type="text"
+                color="default"
+                label="Pesquisar Disciplina"
+                value={filterValue}
+                onValueChange={setFilterValue}
+              />
+            }
+            bottomContent={
+              <PaginationTable
+                page={page}
+                setPage={setPage}
+                totalPages={totalPages}
+              />
+            }
+            selectedKeys={selectedKeys}
+            onSelectionChange={setSelectedKeys}
+            color="primary"
+            selectionMode="multiple"
+          >
+            <TableHeader>
+              <TableColumn key="name">Nome</TableColumn>
+            </TableHeader>
+            <TableBody items={items}>
+              {(discipline) => (
+                <TableRow key={discipline.disciplineCode}>
+                  <TableCell>{discipline.name}</TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+
+          <Button type="submit" color="primary" className="w-full rounded-md">
+            Atribuir
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
