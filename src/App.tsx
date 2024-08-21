4d56f0180c1e8201a3ee348c2d6f7ee0ac035a99
Jonas Dias <zdias1413@gmail.com>
2024-08-20 23:18:50 -0300

    feat: add side menu
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ErrorModal } from "@components/Common/ErrorModal";
 import { SuccessModal } from "@components/Common/SuccessModal";
 import { AuthProvider } from "@contexts/AuthContext";
 import { ErrorProvider } from "@contexts/ErrorContext";
+import { SideMenuProvider } from "@contexts/SideMenuContext";
 import { SuccessProvider } from "@contexts/SuccessContext";
 import { AppRoutes } from "AppRoutes";
 import { BrowserRouter } from "react-router-dom";
@@ -9,9 +10,11 @@ import { BrowserRouter } from "react-router-dom";
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <AuthProvider>
-      <SuccessProvider>
-        <ErrorProvider>{children}</ErrorProvider>
-      </SuccessProvider>
+      <SideMenuProvider>
+        <SuccessProvider>
+          <ErrorProvider>{children}</ErrorProvider>
+        </SuccessProvider>
+      </SideMenuProvider>
     </AuthProvider>
   );
 };
---
+++ src/components/Common/SideMenu.tsx
@@ -0,0 +1,46 @@
+import { useSideMenu } from "@hooks/useSideMenu";
+import { useState } from "react";
+import { MenuItem } from "types/menu";
+
+interface Props {
+  menuItems?: MenuItem[];
+}
+
+export function SideMenu({ menuItems }: Props) {
+  const [selectedTab, setSelectedTab] = useState("");
+  const { isMenuOpen, setIsMenuOpen } = useSideMenu();
+
+  return (
+    <div
+      className={`bg-amber-500 fixed left-0 z-10 transition-all duration-300 p-4 min-h-screen h-full overflow-y-auto
+        ${isMenuOpen ? "w-64" : "hidden lg:block lg:w-16"}`}
+    >
+      {menuItems?.map((item, index) => (
+        <div
+          key={`${item.title}-${index}`}
+          onClick={() => {
+            item.onClick();
+            setSelectedTab(item.title);
+            setIsMenuOpen(false);
+          }}
+          className="my-2"
+        >
+          <div
+            className={`border-2 border-transparent cursor-pointer rounded-lg flex items-center p-2 ${
+              isMenuOpen ? "gap-1 hover:border-white" : "justify-center"
+            } ${isMenuOpen && selectedTab === item.title && "bg-white"}`}
+          >
+            <span
+              className={`text-3xl ${
+                !isMenuOpen && selectedTab === item.title && "text-white"
+              }`}
+            >
+              {item.icon}
+            </span>
+            <span className={isMenuOpen ? "ml-4" : "hidden"}>{item.title}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
