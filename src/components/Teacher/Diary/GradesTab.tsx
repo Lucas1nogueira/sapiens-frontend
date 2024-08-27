d75145191c6ce2e3a78c5f62c1c068bdf14d37e7
Jonas Dias <zdias1413@gmail.com>
2024-08-27 12:52:05 -0300

    wip: working in grades
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>
    Co-authored-by: Ellen Cristina <EllenCristina10@users.noreply.github.com>

--- src/components/Teacher/Diary/GradesTab.tsx
+++ src/components/Teacher/Diary/GradesTab.tsx
@@ -1,19 +1,47 @@
 import { CustomModal } from "@components/Common/CustomModal";
 import { CreateGrade } from "@components/Grade/CreateGrade";
 import { EditGrade } from "@components/Grade/EditGrade";
-import { Button, useDisclosure } from "@nextui-org/react";
-import { useState } from "react";
+import { Icon } from "@iconify/react";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  useDisclosure,
+} from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { findEvaluationsByDisciplineCode } from "services/evaluationService";
+import { Discipline } from "types/discipline";
 import { Evaluation } from "types/evaluation";
 import { Student } from "types/student";
+import { formatDate, formatDateWithHour } from "utils/formatDate";
 
 type Props = {
-  evaluations: Evaluation[];
   students: Student[];
+  discipline: Discipline;
 };
 
-export function GradesTab({ evaluations, students }: Props) {
+const columns = [
+  { key: "name", label: "Nome" },
+  { key: "createdAt", label: "Data de Criação" },
+  { key: "deliveryAt", label: "Data de Entrega" },
+  { key: "addGrade", label: "Lançar Notas" },
+  { key: "editGrade", label: "Editar Notas" },
+];
+
+export function GradesTab({ students, discipline }: Props) {
   const [content, setContent] = useState<JSX.Element>(<></>);
+  const [evaluations, setEvaluations] = useState<Evaluation[]>([]);
   const disclosure = useDisclosure();
+
+  useEffect(() => {
+    findEvaluationsByDisciplineCode(discipline.code)
+      .then((response) => setEvaluations(response.data))
+      .catch((error) => console.log(error));
+  }, [discipline.code]);
 
   const handleCreateGrade = (evaluation: Evaluation) => {
     setContent(<CreateGrade students={students} evaluation={evaluation} />);
@@ -26,20 +54,49 @@ export function GradesTab({ evaluations, students }: Props) {
   };
 
   return (
-    <div className="flex flex-col gap-2">
-      {evaluations.map((evaluation) => (
-        <div key={evaluation.id} className="flex flex-col gap-2 mb-4">
-          <h3 className="text-lg font-semibold">{evaluation.name}</h3>
-          <Button onClick={() => handleCreateGrade(evaluation)}>
-            Lançar Notas
-          </Button>
-          <Button onClick={() => handleEditGrade(evaluation)}>
-            Editar Notas
-          </Button>
-        </div>
-      ))}
+    <>
+      <Table aria-label="Tabble with all evaluations">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={evaluations}>
+          {(evaluation) => (
+            <TableRow key={evaluation.id}>
+              <TableCell>{evaluation.name}</TableCell>
+              <TableCell>
+                {formatDate(evaluation.createdAt) ?? "Sem Data"}
+              </TableCell>
+              <TableCell>
+                {formatDateWithHour(evaluation.deliveryAt) ?? "Sem Data"}
+              </TableCell>
+              <TableCell>
+                <Button
+                  color="primary"
+                  variant="ghost"
+                  onClick={() => handleCreateGrade(evaluation)}
+                  isIconOnly
+                >
+                  <Icon icon="gridicons:create" width={24} />
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button
+                  color="primary"
+                  variant="ghost"
+                  onClick={() => handleEditGrade(evaluation)}
+                  isIconOnly
+                >
+                  <Icon icon="wpf:create-new" width={24} />
+                </Button>
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
 
       <CustomModal useDisclosure={disclosure} content={content} size="full" />
-    </div>
+    </>
   );
 }
