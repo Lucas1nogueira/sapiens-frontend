ba7ffaf6fd3f421f0f42bfd23e4b3d63e0c42e77
Jonas Dias <zdias1413@gmail.com>
2024-08-27 12:52:15 -0300

    wip: working in diary
    Co-authored-by: Lucas Nogueira <Lucas1nogueira@users.noreply.github.com>
    Co-authored-by: Kaio Fernandes <kaiofs122@users.noreply.github.com>
    Co-authored-by: Ellen Cristina <EllenCristina10@users.noreply.github.com>

--- src/components/Common/ServiceCard.tsx
+++ src/components/Common/ServiceCard.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function ServiceCard({ title, description, icon }: Props) {
   return (
-    <div className="border-1 border-zinc-500 rounded-lg px-2 py-8 bg-white hover:bg-blue-600 w-72 h-80 cursor-pointer text-black hover:text-white text-center">
+    <div className="border-1 border-zinc-500 rounded-lg px-2 py-8 bg-white hover:bg-blue-600 w-80 h-80 cursor-pointer text-black hover:text-white text-center">
       <div className="w-28 mx-auto py-4">{icon}</div>
       <div>
         <h1>{title}</h1>
